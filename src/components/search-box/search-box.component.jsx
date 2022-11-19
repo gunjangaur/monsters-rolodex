99539661be56863ecf01fe62ab213d931becf513
gunjangaur <gunjan.gaur0809@gmail.com>
2022-11-19 21:35:30 +0530

    adding files for github pages

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,10 @@
+import './seach-box.styles.css'
+
+export const SearchBox = ({ placeholder, handleChange }) => (
+  <input
+    className='search'
+    type='search'
+    placeholder={placeholder}
+    onChange={handleChange}
+  ></input>
+);
